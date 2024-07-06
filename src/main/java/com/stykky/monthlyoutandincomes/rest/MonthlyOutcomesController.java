7ebe0763312930a171f0b9c57fe77a03b1a6aef9
Cristian Stocco <2491996+cristianstocco@users.noreply.github.com>
2024-07-06 14:46:18 +0200

    added constants for hello_world phrase

--- src/main/java/com/stykky/monthlyoutandincomes/rest/MonthlyOutcomesController.java
+++ src/main/java/com/stykky/monthlyoutandincomes/rest/MonthlyOutcomesController.java
@@ -3,6 +3,7 @@
  */
 package com.stykky.monthlyoutandincomes.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController()
 public class MonthlyOutcomesController {
+	
+	private final String HELLO_WORLD = "HelloWorld! The user is '%s', within the team '%s' :)";
+	
+	@Value("${user.name}")
+	private String USER_NAME;
+	
+	@Value("${user.team}")
+	private String USER_TEAM;
 
 	@GetMapping("/")
 	public String hello() {
-		return "HelloWorld!";
+		return String.format( HELLO_WORLD, USER_NAME, USER_TEAM );
 	}
 	
 }
