efbb669ea5cd4f7537578cfffe73abf1d8f38ada
Cristian Stocco <2491996+cristianstocco@users.noreply.github.com>
2024-07-12 16:54:14 +0200

    added find resource using rest for incomes

--- src/main/java/com/stykky/monthlyoutandincomes/rest/MonthlyIncomesController.java
+++ src/main/java/com/stykky/monthlyoutandincomes/rest/MonthlyIncomesController.java
@@ -3,8 +3,19 @@
  */
 package com.stykky.monthlyoutandincomes.rest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stykky.monthlyoutandincomes.dao.InvestmentDAO;
+import com.stykky.monthlyoutandincomes.dao.WageDAO;
+import com.stykky.monthlyoutandincomes.model.Investment;
+import com.stykky.monthlyoutandincomes.model.Wage;
 
 /**
  * made by stykky
@@ -12,10 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MonthlyIncomesController {
+	
+	@Autowired
+	private WageDAO wageDAO;
+	@Autowired
+	private InvestmentDAO investmentDAO;
 
+	/*
+	 * retrieves all the incomes, from wages and investments
+	 */
 	@GetMapping( "/in/getAll" )
-	public String getAll() {
-		return "the incomes for the monthly controller";
+	public ResponseEntity<Object> getAll() {
+		Map<String, Object> response = new HashMap<>();
+		
+		List<Wage> wages = wageDAO.findAll();
+		List<Investment> investments = investmentDAO.findAll();
+		
+		response.put("wages", wages);
+		response.put("investments", investments);
+		
+		return ResponseEntity.ok( response );
 	}
 	
 }
