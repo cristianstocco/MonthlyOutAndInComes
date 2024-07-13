8f3405aebd6f534d389ad02845c8d3a006d670c7
Cristian Stocco <2491996+cristianstocco@users.noreply.github.com>
2024-07-13 05:59:39 +0200

    added rest resource for monthly outcomes

--- src/main/java/com/stykky/monthlyoutandincomes/rest/MonthlyOutcomesController.java
+++ src/main/java/com/stykky/monthlyoutandincomes/rest/MonthlyOutcomesController.java
@@ -3,8 +3,19 @@
  */
 package com.stykky.monthlyoutandincomes.rest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stykky.monthlyoutandincomes.dao.OtherDAO;
+import com.stykky.monthlyoutandincomes.dao.ShoppingDAO;
+import com.stykky.monthlyoutandincomes.model.Other;
+import com.stykky.monthlyoutandincomes.model.Shopping;
 
 /**
  * made by stykky
@@ -12,10 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController()
 public class MonthlyOutcomesController {
+	
+	@Autowired
+	private ShoppingDAO shoppingDAO;
+	@Autowired
+	private OtherDAO otherDAO;
 
 	@GetMapping( "/out/getAll" )
-	public String getAll() {
-		return "the outcomes for the monthly controller";
+	public ResponseEntity getAll() {
+		Map<String, Object> response = new HashMap<>();
+		
+		List<Shopping> shoppings = shoppingDAO.findAll();
+		List<Other> others = otherDAO.findAll();
+		
+		response.put( "shoppings", shoppings );
+		response.put( "others", others );
+		
+		return ResponseEntity.ok( response );
 	}
 	
 }
